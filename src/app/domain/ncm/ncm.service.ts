882d8f2df1a6cf16147da835f7655402eb941412
Douglas Eleutério <douglas.versato@gmail.com>
2024-05-17 10:57:08 -0300

    Remoção de menus desnecessários Inclusão de mais NCMS de exemplos

--- src/app/domain/ncm/ncm.service.ts
+++ src/app/domain/ncm/ncm.service.ts
@@ -13,7 +13,11 @@ export class NcmService {
   private loadList() {
     this.ncm.push(
       { codigo: '22030000', inicio: new Date('2024-01-04'), fim: new Date('2025-01-01') },
-      { codigo: '96190000', inicio: new Date('2024-01-04'), fim: new Date('2025-01-01') }
+      { codigo: '96190000', inicio: new Date('2024-01-04'), fim: new Date('2025-01-01') },
+      { codigo: '34025000', inicio: new Date('2024-01-04'), fim: new Date('2025-01-01') },
+      { codigo: '33051000', inicio: new Date('2024-01-04'), fim: new Date('2025-01-01') },
+      { codigo: '96032900', inicio: new Date('2024-01-04'), fim: new Date('2025-01-01') },
+      { codigo: '34029090', inicio: new Date('2024-01-04'), fim: new Date('2025-01-01') }
     );
   }
 
