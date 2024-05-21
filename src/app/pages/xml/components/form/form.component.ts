df2281c244962454a85886259c8beac591bcdeb8
Douglas Eleuterio <douglas.versato@gmail.com>
2024-05-21 13:36:17 -0300

    Tradução da página de upload

--- src/app/pages/xml/components/form/form.component.ts
+++ src/app/pages/xml/components/form/form.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { MessageService } from 'primeng/api';
-import { AlertService } from '../../../../core/ui/notifications/alert.service';
-import { TranslateService } from '@ngx-translate/core';
 import { EnvService } from '../../../../env/env.service';
+import { FileUpload } from 'primeng/fileupload';
 
 @Component({
   selector: 'app-upload-form',
@@ -16,12 +15,7 @@ export class FormComponent {
 
   uploadedFiles: any[] = [];
 
-  constructor(
-    private envService: EnvService,
-    private alertService: AlertService,
-    private translateService: TranslateService,
-    private messageService: MessageService
-  ) {
+  constructor(private envService: EnvService, private messageService: MessageService) {
     this.urlUpload = `${this.envService.environment.baseUrl}${this.path}`;
   }
 
@@ -30,7 +24,20 @@ export class FormComponent {
       this.uploadedFiles.push(file);
     }
 
-    this.alertService.defaultSuccess(this.translateService.instant('solicitacao.message.success'.toUpperCase()));
-    this.messageService.add({ severity: 'info', summary: 'File Uploaded', detail: '' });
+    // this.alertService.defaultSuccess(this.translateService.instant('solicitacao.message.success'.toUpperCase()));
+    this.messageService.add({
+      severity: 'info',
+      summary: this.uploadedFiles.length > 1 ? 'Arquivos enviados' : 'Arquivo enviado',
+      detail: ''
+    });
+    this.uploadedFiles = [];
+  }
+
+  envioError(upload: FileUpload) {
+    this.messageService.add({
+      severity: 'error',
+      summary: 'Erro no envio do arquivo',
+      detail: ''
+    });
   }
 }
