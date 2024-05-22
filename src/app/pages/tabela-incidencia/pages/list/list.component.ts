34f3b3bd39a14dedbe148406a4df504cc507d45a
Douglas Eleuterio <douglas.versato@gmail.com>
2024-05-22 19:59:16 -0300

    Componente, Página e Estilo

---
+++ src/app/pages/tabela-incidencia/pages/list/list.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { PaginatorComponent } from '../../../../core/ui/components/pagination/paginator.component';
+import { AlertService } from '../../../../core/ui/notifications/alert.service';
+import { TranslateService } from '@ngx-translate/core';
+import { AppBreadcrumbService } from '../../../../layouts/atlantis/app.breadcrumb.service';
+import { ValidationService } from '../../../../core/ui/notifications/validation.service';
+import { LoadingService } from '../../../../domain/loading/loading.service';
+import { AppMenuModel } from '../../../../domain/menu/app-menu.model';
+import { finalize } from 'rxjs/operators';
+import { TabelaAliquotaDiferenciadaService } from '../../../../domain/tabela-incidencia/tabela-aliquota-diferenciada.service';
+import { TabelaAliquotaDiferenciada } from '../../../../domain/tabela-incidencia/tabela-aliquota-diferenciada.model';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss']
+})
+export class ListComponent extends PaginatorComponent {
+  tableData: TabelaAliquotaDiferenciada[] = [];
+
+  constructor(
+    private service: TabelaAliquotaDiferenciadaService,
+    private translateService: TranslateService,
+    private breadcrumbService: AppBreadcrumbService,
+    private validationService: ValidationService,
+    private loadingService: LoadingService,
+    private alertService: AlertService
+  ) {
+    super('PaginationTabelaIncidencia');
+    this.breadcrumbService.setItems([
+      AppMenuModel.itemTabelaAliquotaDiferenciada,
+      {
+        label: 'tabela_aliquota_diferenciada.title.page'
+      }
+    ]);
+  }
+
+  fetch(): void {
+    this.loadingService.startLoading();
+    this.service
+      .paginate(this.pagination)
+      .pipe(
+        finalize(() => {
+          this.loadingService.stopLoading();
+        })
+      )
+      .subscribe(result => {
+        this.tableData = result.content;
+        this.pagination.totalRecords = result.totalElements;
+      });
+  }
+}
