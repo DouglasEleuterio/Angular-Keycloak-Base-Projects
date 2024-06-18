586aae2d03b90b4f5b67722f08f38b5629f1d8d7
Douglas Eleutério <douglas.versato@gmail.com>
2024-06-18 13:09:06 -0300

    Visualização de genero

--- src/app/domain/cliente/cliente.ts
+++ src/app/domain/cliente/cliente.ts
@@ -1,7 +1,13 @@
 import { BaseActiveEntity } from '../../core/domain/base.active.entity';
 
+enum EGenero {
+  MASCULINO = 'Masculino',
+  FEMININO = 'Feminino'
+}
+
 export class Cliente extends BaseActiveEntity {
   nome: string;
   email: string;
   cpf: string;
+  genero: EGenero;
 }
