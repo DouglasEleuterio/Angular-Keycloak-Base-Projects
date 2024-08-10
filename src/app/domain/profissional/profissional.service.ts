5accc62243d39017cd860960c43f678be3c49459
Douglas Eleutério <douglas.versato@gmail.com>
2024-08-10 10:02:37 -0300

    Mantendo padrão de acesso aos eventos

---
+++ src/app/domain/profissional/profissional.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Profissional } from './profissional.model';
+import { BaseActiveService } from '../../core/domain/base.active.service';
+import { HttpClient } from '@angular/common/http';
+import { EnvService } from '../../env/env.service';
+import { from, QueryBuilder } from '../../core/api/select/select';
+import { ProcedimentoCreateRequest } from '../procedimento/create/procedimento-create-request-model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfissionalService extends BaseActiveService<Profissional, number> {
+  constructor(http: HttpClient, envService: EnvService) {
+    super(http, envService, 'profissional');
+  }
+
+  carregarProfissionais(profissionais: Profissional[]) {
+    const query = from<ProcedimentoCreateRequest>()
+      .select((u: Profissional) => [u.nome, u.id])
+      .where(u => u.eq('situacao', 'true'))
+      .asc(x => x.nome)
+      .getQuery();
+
+    const params: Record<string, string> = query.build();
+    this.http
+      .get<Profissional[]>(`${this.getBaseUrl()}/select`, {
+        params: params
+      })
+      .subscribe(value => {
+        value.forEach(p => profissionais.push(p));
+      });
+  }
+}
