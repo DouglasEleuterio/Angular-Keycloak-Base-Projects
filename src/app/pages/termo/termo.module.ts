8c3152da6131ce6048188e79bda1da916ab34c54
Douglas <douglas-negocios@hotmail.com>
2024-09-08 22:55:54 -0300

    Importação de Componentes; Rota para Botão de Novo Termo;

--- src/app/pages/termo/termo.module.ts
+++ src/app/pages/termo/termo.module.ts
@@ -1,13 +1,36 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SharedModule } from '../../shared/shared.module';
 import { ListComponent } from './pages/list/list.component';
 import { TermoRoutingModule } from './termo-routing.module';
 import { DetailComponent } from './pages/detail/detail.component';
 import { EditorComponent } from '../../shared/editor-component/editor/editor.component';
+import { SharedModule } from '../../shared/shared.module';
+import { TableModule } from 'primeng/table';
+import { UIModule } from '../../core/ui/ui.module';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+import { DockModule } from 'primeng/dock';
+import { FormComponent } from './components/form/form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DropdownModule } from 'primeng/dropdown';
+import { RxReactiveFormsModule } from '@rxweb/reactive-form-validators';
+import { NewComponent } from './pages/new/new.component';
 
 @NgModule({
-  declarations: [ListComponent, DetailComponent],
-  imports: [CommonModule, SharedModule, TermoRoutingModule, EditorComponent]
+  declarations: [ListComponent, DetailComponent, FormComponent, NewComponent],
+  imports: [
+    CommonModule,
+    TermoRoutingModule,
+    EditorComponent,
+    SharedModule,
+    TableModule,
+    UIModule,
+    ButtonModule,
+    RippleModule,
+    DockModule,
+    ReactiveFormsModule,
+    DropdownModule,
+    RxReactiveFormsModule
+  ]
 })
 export class TermoModule {}
